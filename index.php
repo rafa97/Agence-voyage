e5dd40a3ec1266775010e3fd19e482cc6b1ca893
simplon <patrickdealmeida89000@gmail.com>
2016-07-22 10:35:12 +0200

    Modification du css / suppresion video

--- index.php
+++ index.php
@@ -1,17 +1,28 @@
 <!DOCTYPE html>
-<html>
-  <head>
-    <meta charset="utf-8">
-    <title>petit test</title>
-  </head>
-  <body>
-    <!--Video de fond  -->
-    <video autoplay loop poster="img/LosRoques.jpg" id="bgvid">
-      <source src="video/Los_Roques.webm" type="video/webm">
-      <source src="video/Los_Roques.mp4" type="video/mp4">
-    </video>
-    <!--Video de fond -->
-    <!--  -->
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Index Vidéo</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+​
+​
+<video autoplay loop poster="img/LosRoques.jpg" id="bgvid">
+    <source src="video/LosRoques.webm" type="video/webm">
+    <source src="video/LosRoques.mp4" type="video/mp4">
+</video>
+​
+​
+<header>
+  <div id="global">
+    <h1>Bienvenue sur Fly</h1>
+        <p>
+          Veuillez remplir le formulaire afin de choisir le meilleur voyage pour vous.
 
-  </body>
+        </p>
+  </div>
+</header>
+​
+</body>
 </html>
