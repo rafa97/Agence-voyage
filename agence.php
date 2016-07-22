7a325dec6bde0f893dff4362bce6e3ccba7b9d81
simplon <patrickdealmeida89000@gmail.com>
2016-07-22 11:40:46 +0200

    rajout des fils modif

--- agence.php
+++ agence.php
@@ -1,49 +1,49 @@
 <?php
 
-  session_start();
-  $_SESSION['nom'] = $_POST['nom'];
-  $_SESSION['prenom'] = $_POST['prenom'];
-  $_SESSION['civiliteMme'] = $_POST['civiliteMme'];
-  $_SESSION['civiliteMr'] = $_POST['civiliteMr'];
- ?>
+session_start();
+$_SESSION['nom'] = $_POST['nom'];
+$_SESSION['prenom'] = $_POST['prenom'];
+$_SESSION['civiliteMme'] = $_POST['civiliteMme'];
+$_SESSION['civiliteMr'] = $_POST['civiliteMr'];
+?>
 
 <!DOCTYPE html>
 <html>
-  <head>
-    <meta charset="utf-8">
-    <title></title>
-  </head>
-  <body>
-    <?php
+<head>
+  <meta charset="utf-8">
+  <title></title>
+</head>
+<body>
+  <?php
 
-        echo $_POST['voyage-excursion'];
+  echo $_POST['voyage-excursion'];
 
-     ?>
+  ?>
 
-      <!-- case voyage -->
-    <form action="recapitulatif.php" method="get">
-      Choix voyage
-      <select>
-        <option value="voyageCaraibes">Les Caraïbes - super - 3500 € par pers</option>
-        <option value="voyageAsieSud">L'Asie du sud-est - génial - 4000 € par pers</option>
-        <option value="voyageEtatsUnis">Les Etats-Unis - trop fort - 4500 € par pers</option>
-      </select><br>
-      Nombres de personnes :<input type="text" name="nombrePers"/><br>
-      <input type="submit" value="Estimation"/>
-    </form>
+  <!-- case voyage -->
+  <form action="recapitulatif.php" method="get">
+    Choix voyage
+    <select>
+      <option value="voyageCaraibes">Les Caraïbes - super - 3500 € par pers</option>
+      <option value="voyageAsieSud">L'Asie du sud-est - génial - 4000 € par pers</option>
+      <option value="voyageEtatsUnis">Les Etats-Unis - trop fort - 4500 € par pers</option>
+    </select><br>
+    Nombres de personnes :<input type="text" name="nombrePers"/><br>
+    <input type="submit" value="Estimation"/>
+  </form>
 
-      <!-- case excursion -->
+  <!-- case excursion -->
 
-    <form action="recapitulatif.php" method="post">
-      Choix excursion
-      <select>
-        <option value="escursionAcropole">Visite Acropole - 250 € par pers</option>
-        <option value="excursionValleNil">Visite Vallée du Nil - 300 € par pers</option>
-        <option value="excursionCapadoce">Découverte Capadoce - 350 € par pers</option>
-      </select><br>
-      Nombres de personnes :<input type="text" name="nombrePers"/><br>
-      <input type="submit" value="Estimation"/>
-    </form>
+  <form action="recapitulatif.php" method="post">
+    Choix excursion
+    <select>
+      <option value="escursionAcropole">Visite Acropole - 250 € par pers</option>
+      <option value="excursionValleNil">Visite Vallée du Nil - 300 € par pers</option>
+      <option value="excursionCapadoce">Découverte Capadoce - 350 € par pers</option>
+    </select><br>
+    Nombres de personnes :<input type="text" name="nombrePers"/><br>
+    <input type="submit" value="Estimation"/>
+  </form>
 
-  </body>
+</body>
 </html>
--- index.php
+++ index.php
@@ -19,6 +19,20 @@
     <h1>Bienvenue sur Fly</h1>
         <p>
           Veuillez remplir le formulaire afin de choisir le meilleur voyage pour vous.
+          <form action="agence.php" method="post">
+
+            Civilité
+            <select>
+              <option value="civiliteMme">Mme</option>
+              <option value="civiliteMr">Mr</option>
+            </select><br>
+            <label for="nom">Nom: </label><input type="text" name="nom"/><br>
+            <label for="nom">Prénom: </label><input type="text" name="prenom"/><br>
+            Voyage <input type="radio" name="voyage-excursion" value="voyage"/><br>
+            Excursion <input type="radio" name="voyage-excursion" value="excursion"/><br>
+            <input type="submit" value="Suite"/>
+
+          </form>
 
         </p>
   </div>
